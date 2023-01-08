54d349a22f8b38177317248019a6504efc8838a4
Mikkel O. Laursen <m@ikkel.dk>
2023-01-08 14:47:28 +0100

    Add LSP

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,21 @@
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.setup()
