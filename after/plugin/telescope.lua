8ad995924096c9cc1f580f1abaae12b87cec6002
Mikkel O. Laursen <m@ikkel.dk>
2023-01-26 11:06:30 +0100

    Change some bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,6 +6,6 @@ function search()
     })
 end
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fd', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', search, {})
